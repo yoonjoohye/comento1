f376597e4e06b434a78368383ed9a06e7783f725
yoonjoohye <67yjh00@gmail.com>
2018-11-28 22:53:13 +0900

    package.json 모듈 추가

--- src/main.js
+++ src/main.js
@@ -1,8 +1,6 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import { polyfill } from 'es6-promise'
-polyfill()
-
+import 'es6-promise/auto'
 import Vue from 'vue'
 import App from './App'
 import router from './router'
